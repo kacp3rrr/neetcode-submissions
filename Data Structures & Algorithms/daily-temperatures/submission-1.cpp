db6871349561bd2a41b8318fbf09adb67f53e15c
Kacper <64568015+kacp3rrr@users.noreply.github.com>
2026-09-02 01:42:29 -0400

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> output(temperatures.size());
+        // monotonic stack of indexes in temperatures. we pop when the current days temperature is
+        // hotter than the top of the stack, and continually pop until we either reach the bottom or
+        // reach a day with a hotter temperature. at each pop, update the output for day x as
+        // current index - x
+        stack<int> st;
+        for (int i = 0; i < temperatures.size(); ++i) {
+            while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
+                int index = st.top();
+                st.pop();
+                output[index] = i - index;
+            }
+            st.push(i);
+        }
+        return output;
+    }
+};
