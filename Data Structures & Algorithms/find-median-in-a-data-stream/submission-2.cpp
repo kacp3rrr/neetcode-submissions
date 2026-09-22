04be2ccaebc0417b910dfc8af9683ea221a7500c
Kacper <64568015+kacp3rrr@users.noreply.github.com>
2026-09-21 23:30:53 -0400

    Add: find-median-in-a-data-stream - submission-2

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-2.cpp
@@ -0,0 +1,34 @@
+class MedianFinder {
+private:
+    // maxHeap holds the lower half of elements in the datastream, minHeap
+    // holds the upper half.
+    priority_queue<int, vector<int>, less<int>> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    double median;
+public:
+    MedianFinder() {}
+    
+    void addNum(int num) {
+        maxHeap.push(num);
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+        // rebalance the two heaps (after this, they will either be even size, or maxHeap will have 1 more element, in which
+        // the top of maxHeap is our median)
+        if (minHeap.size() > maxHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+        // compute median
+        int maxTop = maxHeap.empty() ? 0 : maxHeap.top();
+        int minTop = minHeap.empty() ? 0 : minHeap.top();
+        if (maxHeap.size() == minHeap.size()) {
+            median = (1.0 * (maxTop+ minTop) / 2);
+        } else {
+            median = maxTop * 1.0;
+        }
+    }
+    
+    double findMedian() {
+        return median;
+    }
+};
